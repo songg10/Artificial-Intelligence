a252ccff9d51f259cad1fd57d5e100be71f86162
Song Tung Nguyen <51797699+songg10@users.noreply.github.com>
2020-03-31 22:55:37 -0700

    Add files via upload

---
+++ Reversi_AI/src/ca/AI/AI.java
@@ -0,0 +1,4 @@
+package ca.AI;
+
+public class AI {
+}
---
+++ Reversi_AI/src/ca/main/Main.java
@@ -0,0 +1,14 @@
+package ca.main;
+
+import ca.reversi.Board;
+
+import static ca.reversi.Reversi.twoPlayers;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Board b = new Board();
+        twoPlayers(b);
+    }
+}
